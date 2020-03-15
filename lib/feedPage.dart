6dff6ab15372604e0d9ef4207e2ae54024c7e87f
Benantar Abdennour <ha_benantar@esi.dz>
2020-03-15 21:42:04 +0100

    Completed the Add New Meme page and routing to feed

--- lib/feedPage.dart
+++ lib/feedPage.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-
+import 'package:image_picker/image_picker.dart';
 
 class FeedPage extends StatefulWidget {
   @override
@@ -10,6 +12,11 @@ class FeedPage extends StatefulWidget {
 class _FeedPageState extends State<FeedPage> {
   GlobalKey _bottomNavigationBarKey=GlobalKey();
   int selectedIndex=2;
+  void postMeme(){
+    setState(() {
+      selectedIndex=1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +27,7 @@ class _FeedPageState extends State<FeedPage> {
           itemBuilder: (BuildContext context,int index){
             return PostCard();
           },
-        )):(NewPost()),
+        )):(new NewPost(postMeme:postMeme)),
       ),
       bottomNavigationBar: CurvedNavigationBar(
         height: 60,
@@ -163,6 +170,8 @@ class _UserImage extends StatelessWidget {
   }
 }
 class NewPost extends StatelessWidget{
+  final _callBack;
+    NewPost({@required void postMeme()}):_callBack=postMeme;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -170,7 +179,33 @@ class NewPost extends StatelessWidget{
         padding: const EdgeInsets.fromLTRB(5, 0, 5, 2),
         child: Card(
           child: Column(
-            children: <Widget>[_PostDetails(),_AddNew()],
+            children: <Widget>[_NewPostDetails(),
+              _NewPostTitle(),
+              Expanded(child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: AddNew(),
+            )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+
+                  RaisedButton(
+                    color: Color.fromRGBO(140,223,224,0.8),
+                    onPressed: (){
+                      _callBack();
+                    },
+                    child:Row(
+                      children: <Widget>[
+                        Icon(Icons.add_circle_outline,color: Colors.white,),
+                        SizedBox(width: MediaQuery.of(context).size.width*0.02,),
+                        Text('Post',style: TextStyle(fontSize: 15,color: Colors.white),),
+                        SizedBox(width: MediaQuery.of(context).size.width*0.02,),
+                      ],
+                    ),
+                  ),SizedBox(width: MediaQuery.of(context).size.width*0.02,)
+                ],
+              )
+            ],
           ),
         ),
       ),
@@ -178,12 +213,112 @@ class NewPost extends StatelessWidget{
 
   }
 }
-class _AddNew extends StatelessWidget {
+class _NewPostDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Row(
+      children: <Widget>[_UserImage(), _UserNameAndHashTagsNewPost()],
     );
+  }
+}
+class _NewPostTitle extends StatelessWidget {
+  const _NewPostTitle({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle titleTheme = Theme.of(context).textTheme.title;
+    final String title = "";
+
+    return Expanded(
+      flex: 0,
+      child: Padding(
+        padding: const EdgeInsets.only(left:15.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+        Container(
+          height: 50,
+
+          width: MediaQuery.of(context).size.width*0.8,
+          child: TextField(
+            maxLines: 2,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: "Caption it",
+          ),),
+        )
+          ],
+        ),
+      ),
+    );
+  }
+}
+class _UserNameAndHashTagsNewPost extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 5,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Abdenour",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+            Container(
+              height: 30,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Add your hashtags here",
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddNew extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _AddNewState();
+  }
+}
+class _AddNewState extends State<AddNew>{
+  File image;
+  Future getImage () async{
+    var _image=await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      image=_image;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return image==null?
+    Container(
+      color: Colors.grey,
+      child: Center(
+        child: Container(
+          width: 90,
+          height: 90,
+          child:
+          FloatingActionButton(
+            backgroundColor: Colors.grey,
+            onPressed: getImage,
+            elevation: 80,
+            foregroundColor: Color.fromRGBO(140,223,224,1.0),
+            tooltip: 'Pick a meme',
+            child: Icon(Icons.add,size: 30,),
+          )
+        ),
+      ),
+    ):
+    Container(
+        child: Image.file(image,fit: BoxFit.contain,));
   }
 }
 
